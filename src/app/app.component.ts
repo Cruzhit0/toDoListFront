67d01d071a92ed25c03b09eaba69c63f215aa157
cruzhit0 <carlos.cruz@ucb.edu.bo>
2025-03-27 00:22:54 -0400

    se quito la configuracion de server side rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/models/theme.service';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './pages/components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
+  themeService = inject(ThemeService);
   title = 'toDoListFront';
 }
