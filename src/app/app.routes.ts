87cc82f3692b446a503751400aa26d61b46f5dfb
cruzhit0 <carlos.cruz@ucb.edu.bo>
2025-03-27 10:38:04 -0400

    implementacion del interceptor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
-import type { Routes } from "@angular/router"
-import { authGuard } from "./core/guards/auth.guard"
 
 export const routes: Routes = [
     {
@@ -15,21 +13,17 @@ export const routes: Routes = [
         path: "auth/login",
         loadComponent: () => import("./pages/login/login.component").then((m) => m.LoginComponent),
       },
-    
       {
-        path: "**",
-        redirectTo: "dashboard",
+        path: "dashboard",
+        loadComponent: () => import("./pages/dashboard/dashboard.component").then((m) => m.DashboardComponent),
+        // canActivate: [authGuard],
       },
-{
-    path: "dashboard",
-    loadComponent: () => import("./pages/dashboard/dashboard.component").then((m) => m.DashboardComponent),
-    // canActivate: [authGuard],
-  },
-  {
-    path: "tasks",
-    loadComponent: () => import("./pages/task/task.component").then((m) => m.TaskComponent),
-    // canActivate: [authGuard],
-  },
+      {
+        path: "tasks",
+        loadComponent: () => import("./pages/task/task.component").then((m) => m.TaskComponent),
+        // canActivate: [authGuard],
+      },
+
 //   {
 //     path: "tasks/:id",
 //     loadComponent: () =>
