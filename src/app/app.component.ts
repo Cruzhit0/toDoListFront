ebec04fbedb3a421366436ed7d7c59433c035096
cruzhit0 <carlos.cruz@ucb.edu.bo>
2025-03-27 01:04:46 -0400

    se aplico la verificacion de inicio de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject} from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './core/services/theme.service';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './pages/components/header/header.component';
 import { BackgroundComponent } from './pages/components/background/background.component';
-
+import { AuthService } from './core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,18 @@ import { BackgroundComponent } from './pages/components/background/background.co
   styleUrl: './app.component.css'
 })
 
-export class AppComponent implements OnInit {
-  constructor() { }
-  ngOnInit() { }
+export class AppComponent {
+
   themeService = inject(ThemeService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
   title = 'toDoListFront';
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
 }
