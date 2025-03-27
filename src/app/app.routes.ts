7de4e8162ea483f63289fa7a6b3cc5911dff4a9a
cruzhit0 <carlos.cruz@ucb.edu.bo>
2025-03-27 09:45:59 -0400

    pagina dashboard hecho, no me convence

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: "dashboard",
+    loadComponent: () => import("./pages/dashboard/dashboard.component").then((m) => m.DashboardComponent),
+    // canActivate: [authGuard],
+  },
+
+];
